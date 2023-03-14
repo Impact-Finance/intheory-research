3f2c776e3a802b9288d6736700cf816517386133
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2023-03-14 02:14:50 -0600

    wallet modal styles

--- utils/dynamicWalletStyles.tsx
+++ utils/dynamicWalletStyles.tsx
@@ -1,3 +1,18 @@
+const colorMainRgb = '12, 41, 75';
+const colorAccentRgb = '104, 234, 255';
+const colorDarkRgb = '4, 12, 22';
+const colorErrorRgb = '254, 0, 254';
+const textXtraLarge = '2.6rem';
+const textLarge = '2.2rem';
+const textMedium = '1.6rem';
+const textSmall = '1.2rem';
+const textXtraSmall = '1rem';
+const boxBorderRadius = '1.8rem';
+const btnBorderRadius = '0.8rem';
+const fontFamily = 'Jura, sans-serif';
+const modalWidth = '60rem';
+const modalHeight = '35rem';
+
 const dynamicWalletStyles = `
   .typography,
   .typography--primary,
@@ -8,26 +23,30 @@ const dynamicWalletStyles = `
   .search__button--clear,
   p,
   button {
-    font-family: Jura, sans-serif;
-    font-size: large;
+    font-family: ${fontFamily};
+    font-weight: 400;
   }
   .typography--title {
-    color: #68eaff;
-    font-size: 2.4rem;
-    line-height: 3.5rem;
+    color: rgb(${colorAccentRgb});
+    font-size: ${textXtraLarge};
+  }
+  .typography--body-normal {
+    font-size: ${textLarge};
+  }
+  .typography--body-small {
+    font-size: ${textMedium};
   }
   .typography--dynamic-error-1 {
-    color: rgb(254, 0, 254);
+    color: rgb(${colorErrorRgb});
   }
   .typography--button-primary {
-    color: #68eaff;
-    font-weight: 400;
-    font-size: inherit;
+    color: rgb(${colorAccentRgb});
+    font-size: ${textMedium};
     transition: all 0.2s;
   }
   .dynamic-connect-button {
-    background-color: #0c294b;
-    border-radius: 0.8rem;
+    background-color: rgb(${colorMainRgb});
+    border-radius: ${btnBorderRadius};
     border-color: transparent;
     padding: 0.8rem 1.5rem;
     width: fit-content;
@@ -44,252 +63,300 @@ const dynamicWalletStyles = `
     width: calc(100% + var(--borderWidth) * 2);
     background: linear-gradient(
       60deg,
-      #68eaff,
-      #0c294b,
-      #fe00fe,
-      #0c294b,
-      #68eaff
+      rgb(${colorAccentRgb}),
+      rgb(${colorMainRgb}),
+      rgb(${colorErrorRgb}),
+      rgb(${colorMainRgb}),
+      rgb(${colorAccentRgb})
     );
-    border-radius: 0.8rem;
+    border-radius: ${btnBorderRadius};
     z-index: -1;
     animation: animatedGradient 6s ease alternate infinite;
     background-size: 300% 300%;
   }
   .dynamic-connect-button:hover {
-    background-color: rgb(12, 41, 75);
+    background-color: rgb(${colorMainRgb});
     border-color: transparent;
   }
   .dynamic-connect-button:hover > .typography--button-primary {
-    color: rgba(104, 234, 255, 0.75)
+    color: rgba(${colorAccentRgb}, 0.75)
   }
   .dynamic-connect-button:hover::after {
     animation-play-state: paused;
   }
 
   .dynamic-auth-layout__modal {
-    width: 60rem;
-    border-radius: 1.8rem;
+    width: ${modalWidth};
+    border-radius: ${boxBorderRadius};
   }
   .dynamic-auth-layout__container {
-    border-radius: 1.8rem;
-    border-top: 1px solid rgba(104, 234, 255, 0.25);
-    border-bottom: 1px solid rgba(104, 234, 255, 0.25);
+    border-radius: ${boxBorderRadius};
+    border-top: 1px solid rgba(${colorAccentRgb}, 0.5);
+    border-bottom: 1px solid rgba(${colorAccentRgb}, 0.5);
   }
   .vertical-accordion__container {
-    background-color: rgb(4, 12, 22);
-    padding: 2rem;
-    border-radius: 1.8rem;
+    background-image: linear-gradient(to bottom, rgba(${colorDarkRgb}, 0.75), rgba(${colorDarkRgb}, 1));
+    padding: ${textLarge};
+    border-radius: ${boxBorderRadius};
+  }
+  .modal-header {
+    padding-top: 0;
   }
   .wallet-list__scroll-container--full-height {
-    max-height: 40rem !important;
+    max-height: ${modalHeight} !important;
   }
   .search__container {
-    height: 4rem;
-    padding: 1.2rem;
+    height: calc(${textLarge} * 1.8);
+    padding: ${textSmall};
+    border-radius: ${btnBorderRadius};
+  }
+  .search__input {
+    font-size: ${textMedium};
   }
   .search-icon__container {
-    height: 2rem;
-    width: 2rem;
+    height: ${textLarge};
+    width: ${textLarge};
   }
-  
   .icon-button > svg {
-    height: 2.2rem;
-    width: 2.2rem;
+    height: ${textLarge};
+    width:${textLarge};
   }
-
   .wallet-list-item__tile {
-    padding: 1.5rem;
-    gap: 1rem;
+    padding: ${textSmall};
+    gap: ${textXtraSmall};
+    border-top: 1px solid rgba(${colorAccentRgb}, 0);
+    border-bottom: 1px solid rgba(${colorAccentRgb}, 0);
+    border-radius: ${btnBorderRadius};
+    transition: all 0.2s;
+  }
+  .wallet-list-item__tile:hover {
+    border-top: 1px solid rgba(${colorAccentRgb}, 0.25);
+    border-bottom: 1px solid rgba(${colorAccentRgb}, 0.25);
+    background-color: rgba(${colorMainRgb}, 0.15)
   }
   .wallet-list-item__tile > img {
-    width: 2.2rem !important;
-    height: 2.2rem !important;
+    width: ${textLarge} !important;
+    height: ${textLarge} !important;
   }
-
   .badge__container {
     gap: 0.75rem;
     padding: 0.5rem 1rem;
-    border-radius: 0.8rem;
+    border-radius: ${btnBorderRadius};
+    background-color: rgba(${colorMainRgb}, 0.5);
   }
-
   .badge__container > span {
-    font-size: 1.6rem;
+    font-size: ${textMedium};
   }
-
   .badge__dot {
     height: 0.75rem;
     width: 0.75rem;
   }
-
-  .search-instead__container > p:last-of-type {
-    font-size: 1.4rem;
-    margin-top: 0.5rem;
+  .search-instead__container > p {
+    margin: 0.5rem;
   }
-
   .error-container {
-    color: rgb(254, 0, 254);
-    background-color: rgba(254, 0, 254, 0.15)
+    color: rgb(${colorErrorRgb});
+    background-color: transparent;
   }
   .error-container p {
-    color: rgb(254, 0, 254);
+    color: rgb(${colorErrorRgb});
   }
-
-  .footer, 
-  .badge__container {
-    background-color: rgba(12, 41, 75, 0.5);
+  .footer {
+    background-color: rgba(${colorMainRgb}, 0.5);
+    border-radius: ${btnBorderRadius};
   }
-
   .footer__container--icon svg {
-    height: 2.2rem;
-    width: 2.2rem;
+    height: ${textLarge};
+    width: ${textLarge};
   }
-
   .footer__arrow {
-    width: 2.2rem;
-    height: 2.2rem;
+    width: ${textLarge};
+    height: ${textLarge};
   }
   .footer-buttons-item {
-    background-color: rgba(12, 41, 75, 0.25);
+    background-color: rgba(${colorMainRgb}, 0.25);
+    border-radius: ${btnBorderRadius};
   }
   .footer-buttons-item__icon--left {
-    margin-right: 1rem;
+    margin-right: ${textXtraSmall};
   }
   .footer-buttons-item__icon--left img,
   .footer-buttons-item__icon--left svg {
-    width: 2rem;
-    height: 2rem;
+    width: ${textLarge};
+    height: ${textLarge};
   }
-
+  .walletlist-footer-content__typography-wrapper {
+    margin: 2rem 0;
+  }
   .walletlist-footer-content__icon {
     width: 5rem;
-    margin-top: 1rem;
+    margin-top: ${textXtraSmall};
   }
-
   .new-to-web3-buttons__container button {
-    background-color: rgba(12, 41, 75, 0.5);
-    color: #68eaff;
+    background-color: rgba(${colorMainRgb}, 0.5);
+    color: rgb(${colorAccentRgb});
+    border-radius: ${btnBorderRadius};
   }
 
   .pending-signature__container .pending-signature__title {
-    margin-bottom: 1rem;
-  }
-  .pending-signature__container .pending-signature__copy {
-    line-height: 2.2rem;
-  }
-  .powered-by-dynamic__logo {
-    max-width: 10rem;
-  }
-  .icon-button,
-  .info-item__container .info-item__icon-container,
-  .info-item__container .info-item__icon-container svg {
-    width: 2.2rem;
-    height: 2.2rem;
-  }
-  .qrcode__container {
-    width: 20rem;
-    height: 20rem;
-  }
-  .qrcode__icon,
-  .icon-with-spinner__spinner-container,
-  .icon-with-spinner__icon-container,
-  .icon-with-spinner__icon-container img {
-    width: 4rem !important;
-    height: 4rem !important;
-  }
-  .icon-with-spinner__spinner {
-    display: none;
-  }
-  .qr-code-view__scan-issue-message {
-    background-color: rgba(12, 41, 75, 0.5)
-  }
-  .qr-code-view__scan-issue-button {
-    font-size: inherit;
-    color: #68eaff;
+    margin-bottom: ${textXtraSmall};
   }
   .pending-signature__container .pending-signature__copy,
   .qrcode-container__content {
-    max-width: 35rem;
+    line-height: calc(${textLarge} * 1.2);
+    max-width: calc(${modalWidth} * 0.66);
+  }
+  .powered-by-dynamic__logo {
+    max-width: 6rem;
+  }
+  .footer-more-info-content__wrapper {
+    gap: ${textSmall};
+  }
+  .info-item__container .info-item__icon-container,
+  .info-item__container .info-item__icon-container svg {
+    width: ${textLarge};
+    height: ${textLarge};
+  }
+  .info-item__secondary-text {
+    font-size: calc(${textLarge} * 0.85);
+    line-height: ${textLarge};
+  }
+  .icon-button {
+    height: calc(${textLarge} * 0.9);
+    width: calc(${textLarge} * 0.9);
+  }
+  .qrcode__container {
+    width: calc(${modalWidth} * 0.4);
+    height: calc(${modalWidth} * 0.4);
+  }
+  .qrcode__icon,
+  .icon-with-spinner__icon-container,
+  .icon-with-spinner__icon-container img {
+    width: calc(${textMedium} * 2) !important;
+    height: calc(${textMedium} * 2) !important;
+  }
+  .qr-code-view__scan-issue-message {
+    background-color: transparent;
+  }
+  .qr-code-view__scan-issue-button {
+    color: rgb(${colorAccentRgb});
+    font-size: inherit;
   }
   .wallet-help-link__anchor {
-    background-color: rgba(12, 41, 75, 0.5);
-    gap: 1rem;
+    background-color: rgba(${colorMainRgb}, 0.5);
+    gap: ${textXtraSmall};
+    margin-bottom: ${textXtraSmall};
     border: none;
+    border-radius: ${btnBorderRadius};
   }
   .wallet-help-link__anchor img {
-    width: 2rem !important;
-    height: 2rem !important;
+    width: ${textLarge} !important;
+    height: ${textLarge} !important;
+  }
+  .wallet-help-link__anchor p {
+    color: white;
   }
   .copy-button__container {
-    font-size: 1.4rem;
+    font-size: ${textSmall};
   }
   .icon--size-small {
-    width: 2rem;
-    height: 2rem;
+    width: ${textMedium};
+    height: ${textMedium};
   }
 
   .dynamic-widget-inline-controls {
-    max-height: 4rem;
-    background-color: rgba(12, 41, 75, 0.5);
-    border-radius: 0.8rem;
+    max-height: 4.5rem;
+    background-color: rgba(${colorMainRgb}, 0.5);
+    border-radius: ${btnBorderRadius};
   }
   .dynamic-widget-inline-controls__network-picker,
   .account-control__container {
-    border-radius: 0.8rem;
-    color: rgba(104, 234, 255, 0.75);
-    gap: 1rem;
-    font-size: 1.8rem;
+    border-radius: ${btnBorderRadius};
+    color: rgba(${colorAccentRgb}, 0.75);
+    gap: ${textXtraSmall};
+    font-size: ${textMedium};
     transition: all 0.2s;
   }
   .dynamic-widget-inline-controls__network-picker {
-    border-bottom-right-radius: 0px;
-    border-top-right-radius: 0px;
+    border-bottom-right-radius: 0;
+    border-top-right-radius: 0;
   }
   .account-control__container {
-    border-bottom-left-radius: 0px;
-    border-top-left-radius: 0px;
+    border-bottom-left-radius: 0;
+    border-top-left-radius: 0;
   }
   .account-control__container svg {
-    color: rgba(104, 234, 255, 0.75) !important;
+    color: rgba(${colorAccentRgb}, 0.75) !important;
+  }
+  .dynamic-widget-inline-controls__network-picker > .icon--size-small,
+  .account-control__container > .icon--size-small {
+    height: calc(${textMedium} * 1.25);
+    width: calc(${textMedium} * 1.25);
   }
   .dynamic-widget-inline-controls__network-picker:hover,
   .account-control__container:hover { 
-    background-color: rgba(12, 41, 75, 0.5);
-    filter: opacity(0.75);
+    background-color: rgba(${colorMainRgb}, 0.5);
+    filter: opacity(0.85);
   }
   .dynamic-widget-modal {
-    max-height: 25rem;
-    width: 35rem;
+    max-height: ${modalHeight};
+    width: calc(${modalWidth} * 0.75);
   }
   .dynamic-widget-card {
-    background-color: rgb(4, 12, 22);
+    background-image: linear-gradient(to bottom, rgba(${colorDarkRgb}, 0.75), rgba(${colorDarkRgb}, 1));
+    border: none;
+    border-top: 1px solid rgba(${colorAccentRgb}, 0.5);
+    border-bottom: 1px solid rgba(${colorAccentRgb}, 0.5);
+    border-radius: ${boxBorderRadius};
   }
   .status-dot {
     width: 0.75rem;
     height: 0.75rem;
   }
   .dynamic-widget-header__icon-container{
-    height: 6rem;
+    height: 6.5rem;
+    width: 6.5rem;
+    margin-bottom: 1rem;
   }
   .dynamic-widget-header__network-picker.evm-network-control__container {
     height: 3rem;
   }
+  .dynamic-widget-header__balance {
+    margin-bottom: 0;
+    font-size: ${textMedium};
+  }
   .single-wallet-buttons .button--primary {
-    background-color: rgba(12, 41, 75, 0.5);
+    background-color: rgba(${colorMainRgb}, 0.5);
     border: none;
+    border-radius: ${btnBorderRadius};
   }
   .action-list__container {
-    background-color: rgba(12, 41, 75, 0.5);
+    background-color: rgba(${colorMainRgb}, 0.85);
     backdrop-filter: blur(10px);
+    border-radius: ${btnBorderRadius};
   }
   .network .network__title-icon {
-    width: 1.8rem;
-    height: 1.8rem;
+    width: ${textMedium};
+    height: ${textMedium};
   }
-  .network__title-container span {
-    font-size: 1.4rem;
+  .dynamic-widget-inline-controls__network-picker-list {
+    top: 3rem;
   }
-  .evm-network-control__container--error span {
-    font-size: 1.6rem;
+  .network-not-supported__continue {
+    background-color: rgba(${colorMainRgb}, 0.5);
+    border-radius: ${btnBorderRadius};
+    border: none;
+  }
+  .network-not-supported__network-container > img {
+    height: 6.5rem !important;
+    width: 6.5rem !important;
+  }
+  .evm-network-control__container--error {
+    color: rgb(${colorErrorRgb});
+    background-color: rgba(${colorMainRgb}, 0.75);
+  }
+  .network-not-supported__log-out {
+    font-size: ${textSmall};
   }
 
   @keyframes animatedGradient {
